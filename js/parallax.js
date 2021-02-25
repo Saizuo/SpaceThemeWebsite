c8a21e030f1cec5fa946399192517f09480d3c0f
Sajid Jahabar Ali <sajidjahabarali@gmail.com>
2021-02-25 12:36:13 +0000

    added stars to parallax background

--- js/parallax.js
+++ js/parallax.js
@@ -1,3 +1,13 @@
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+    this.querySelectorAll('.layer').forEach(layer => {
+        const speed = layer.getAttribute('item-speed');
+        const midpointX = window.innerWidth/2;
+        const midpointY = window.innerHeight/2;
 
-var scene = document.getElementById('home-container');
-var parallaxInstance = new Parallax(scene);
+        const x = (midpointX - e.pageX)*parseFloat(speed);
+        const y = (midpointY - e.pageY)*parseFloat(speed);
+        
+        layer.style.transform = `translate(${x}px, ${y}px)`;
+    });
+}
